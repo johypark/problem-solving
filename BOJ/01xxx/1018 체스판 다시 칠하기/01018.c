6588b91b1e490d1da0f9f8031947bedb481e558c
Joohyung Park <johy@dabyeol.com>
2021-04-15 16:33:56 +0900

    Remove VLA

--- BOJ/01xxx/1018 체스판 다시 칠하기/01018.c
+++ BOJ/01xxx/1018 체스판 다시 칠하기/01018.c
@@ -4,12 +4,12 @@
 
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 enum { BLACK = 'B', WHITE = 'W' };
 
 // Get minimum repaint count on 8x8 board starting at (x, y)
-size_t get_repaint_count(size_t x, size_t y, size_t column,
-                         bool board[][column]) {
+size_t get_repaint_count(bool** board, size_t x, size_t y) {
   size_t count = 0;
   bool color = true;
 
@@ -28,13 +28,13 @@ size_t get_repaint_count(size_t x, size_t y, size_t column,
 }
 
 // Get minimum repaint count on board
-size_t get_min_repaint_count(size_t row, size_t column, bool board[][column]) {
+size_t get_min_repaint_count(bool** board, size_t row, size_t column) {
   size_t count = 64;
   size_t repaint_count;
 
   for (size_t i = 0; i <= row - 8; i++)
     for (size_t j = 0; j <= column - 8; j++) {
-      repaint_count = get_repaint_count(i, j, column, board);
+      repaint_count = get_repaint_count(board, i, j);
       if (count > repaint_count) count = repaint_count;
     }
 
@@ -43,14 +43,15 @@ size_t get_min_repaint_count(size_t row, size_t column, bool board[][column]) {
 
 int main(int argc, char* argv[]) {
   size_t n, m;
+  bool** board;
+  char color;
 
   scanf("%zu %zu", &n, &m);
   getchar();
 
-  bool board[n][m];
-  char color;
-
+  board = malloc(sizeof(bool*) * n);
   for (size_t i = 0; i < n; i++) {
+    board[i] = malloc(sizeof(bool) * m);
     for (size_t j = 0; j < m; j++) {
       scanf("%c", &color);
       board[i][j] = color == BLACK;
@@ -59,5 +60,8 @@ int main(int argc, char* argv[]) {
     getchar();
   }
 
-  printf("%zu\n", get_min_repaint_count(n, m, board));
+  printf("%zu\n", get_min_repaint_count(board, n, m));
+
+  for (size_t i = 0; i < n; i++) free(board[i]);
+  free(board);
 }
