3e53fbbf1e16525aa8b873467549f5f942b24c4c
Joohyung Park <johy@dabyeol.com>
2021-11-22 16:33:35 +0900

    Minor changes

--- BOJ/03xxx/03053 택시 기하학/03053.cc
+++ BOJ/03xxx/03053 택시 기하학/03053.cc
@@ -1,7 +1,9 @@
+#define _USE_MATH_DEFINES
+
+#include <cmath>
 #include <iostream>
 
 enum { PRECISION = 6 };
-const double PI = 3.141592653589793238463;
 
 using namespace std;
 
@@ -15,7 +17,7 @@ int main() {
   cout.precision(PRECISION);
   cout << fixed;
 
-  cout << r * r * PI << '\n';
+  cout << r * r * M_PI << '\n';
   cout << r * r * 2.0 << endl;
 
   // Reset cout flags
